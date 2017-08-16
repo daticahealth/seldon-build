bd1720fc478e82d7fd4e2f28b0dae41260f0d156
R. Allan White <allan@catalyze.io>
2017-08-16 09:58:28 -0700

    Testing latest build... a few items may need to be gitignored.
    Some really large, persistent images that keep coming back from the dead removed. Where are they coming from?

--- assets/js/leadership-gallery.js
+++
@@ -1,6 +0,0 @@
-// leadership gallery tab management
-$("[data-tabs]").on("change.zf.tabs", function(event, tab) {
-    var tabID = $(tab).attr('id');
-    // console.log("ID is: " + tabID);
-    $(".lg-nav--item").removeClass("is-first");
-});
