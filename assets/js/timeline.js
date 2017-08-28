6e33cfcde66331d653e78691ec117231c67edf89
R. Allan White <allan@catalyze.io>
2017-08-28 10:06:17 -0700

    Foundation 6.4.3 update https://github.com/catalyzeio/seldon/pull/239

--- assets/js/timeline.js
+++
@@ -1,8 +0,0 @@
-$("document").ready(function(){
-    var timeline = $("#timeline");
-    var timelineHeight = timeline.height();
-    var line = $("#line");
-
-    // This sets the height on the timeline height. This way no matter how tall the content gets, we'll always have the correct height
-    line.css({height: timelineHeight})
-});
